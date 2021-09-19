982019f9f8557c901e8083f85baaad6015685494
fardin <abirfardin@gmail.com>
2021-09-19 12:31:42 +0600

    pin matched

--- appCmntLess.js
+++ appCmntLess.js
@@ -8,31 +8,20 @@ function getPin(){
         return pin;
     } 
     else{
-        // console.log('got a 3 digit and calling again', pin);
         return getPin();
     }
 }
 
 
 function generatePin(){
-    // const pin = Math.round(Math.random()* 10000);
-    //  console.log('generating');
     const pin = getPin();
-    // console.log(pin);
     document.getElementById('display-pin').value = pin;
 }
 
 document.getElementById('key-pad').addEventListener('click', function(event){
-    //using (event bubble) or (event-deligate)
-
-
-    // console.log('any key clicked');
-    // console.log(event.target.innerText);
-    const number = event.target.innerText; //used (event-bubble) or (event-deligate)
-    // typeof --string---parse kore output deya jaite pare 
+    const number = event.target.innerText; //used 
     const calcInput = document.getElementById('typed-numbers');
     if(isNaN(number)){ // if not a number
-        // console.log(number);  
         if(number == 'C'){
             calcInput.value = '';
         }
@@ -41,29 +30,23 @@ document.getElementById('key-pad').addEventListener('click', function(event){
         const calcInput = document.getElementById('typed-numbers');
         const previousNumber = calcInput.value;
         const newNumber = previousNumber + number;
-        // calc.innerText = number; vul;
         calcInput.value = newNumber;
     }
     
-});
-
+}); //added a semicolon;
+///////////////////////////////////////////
+//////////////////////////////////////////
 function verifyPin(){
-    //  console.log('pin verifying');
     const pin = document.getElementById('display-pin').value;
     const typedNumbers = document.getElementById('typed-numbers').value;
     const successMassage = document.getElementById('notify-success');
     const failError = document.getElementById('notify-fail');
 
     if(pin == typedNumbers){
-         // console.log('matched! YaY');
-        // const successMassage = document.getElementById('notify-success');
         successMassage.style.display = "block";
-        failError.style.display = "none";
-        
+        failError.style.display = "none";       
     }
     else{
-        // console.log('oops!');
-        // const failError = document.getElementById('notify-fail');
         successMassage.style.display = "none";
         failError.style.display = "block";
        
